c31bf1ea752221448d42cf8433106fdb77925cc0
SDCmobility <sdcmobility1@gmail.com>
2016-12-07 09:22:30 +0100

    comentario

---
+++ models/alumno.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var alumnoSchema = new Schema({
+		num_lista: Number,
+		nombre: String,
+		apellido1: String,
+		apellido2: String,
+		ciclo: {
+			type: String,
+			enum: ['SMR', 'ASIR']
+		},
+		curso: Number,
+		año: Number,
+	});
+	
+	module.exports = mongoose.model('Alumno', alumnoSchema);
